9e040db586c59e993d227058a5d24dbfe2f312e3
sebastian pinto <sebastian.pinto@correo.unimet.edu.ve>
2024-05-01 13:35:36 -0400

    Create Clase 2.py

---
+++ Clase 2.py
@@ -0,0 +1,11 @@
+name=input("ingrese su nombre")
+n1=float(input("Ingrese el primer numero"))
+n2=float(input("ingrse el segundo numero"))
+
+if n2==0:
+    print("error")
+else:
+    print(n1/n2)
+
+
+
