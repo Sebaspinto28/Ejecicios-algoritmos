4a9fdb17264e714f2c2e00353dc5cef2e1c08bd8
sebastian pinto <sebastian.pinto@correo.unimet.edu.ve>
2024-05-01 17:21:29 -0400

    ejercicio3
    listo

---
+++ Ejer3.py
@@ -0,0 +1,8 @@
+#Ejercicio numero pares e impares 
+numero=int(input("Agregar numero:"))
+par= numero%2
+if par==0:
+    print(f"el numero {numero} es par")
+else:
+    print(f"El numero {numero} es impar")
+ 
