bfebcedd63cc3d97100fa1cc7711a628ab05dd72
sebastian pinto <sebastian.pinto@correo.unimet.edu.ve>
2024-05-01 16:51:58 -0400

    2.0
    2.0

--- Ejer2.py
+++ Ejer2.py
@@ -1,8 +1,8 @@
 print("Welcome to pizzeria")
-main_option=int(input("please enter your option\n1. vegetariano\n2. no vegetariano\n"))
+main_option=int(input("opciones de pizza\n1. vegetariano\n2. no vegetariano\nescoja que tipo de pizza desea:"))
 if main_option == 1:
     print("Usted a escogido la pizza vegetariana")
-    ingredientes=int(input("please enter your option\n1. pimiento\n2. tofu\n"))
+    ingredientes=int(input("Los ingredientes a escoger son:\n1. pimiento\n2. tofu\nescoja el que corresponda:"))
     if ingredientes == 1:
         print("Usted a escogido la pizza con queso mazzarella, tomate y pimiento")
     elif ingredientes == 2:
@@ -11,7 +11,7 @@ if main_option == 1:
         print("error")
 elif main_option == 2:
     print("Usted a escogido la pizza no vegetariana")
-    ingredientes=int(input("please enter your option\n1. peperoni\n2. jamon\n3.salmon\n"))
+    ingredientes=int(input("Los ingredientes a escoger son:\n1. peperoni\n2. jamon\n3.salmon\nescoja el que corresponda:"))
     if ingredientes == 1:
         print("Usted a escogido la pizza con queso mazzarella, tomate y peperoni")
     elif ingredientes == 2:
